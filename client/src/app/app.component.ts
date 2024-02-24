8e7a9cc211b4ff589ae5f457bf79a3e56d08311d
darikutelov <dari@kutelov.com>
2024-02-24 14:42:15 +0200

    Lower Angular version, add ngx bootstrap, move angular to https

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (users) => {
+        this.users = users as any[];
+      },
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
+  }
 }
