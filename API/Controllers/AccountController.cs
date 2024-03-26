c99a2ae9598da8a50016a09e03aba8cf06fda4b6
darikutelov <dari@kutelov.com>
2024-03-26 10:39:43 +0200

    Identity Refactoring entity, authentication and seeding

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -8,21 +8,22 @@ using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Controllers;
 
 public class AccountController : BaseApiController
 {
-    private readonly DataContext _context;
+    private readonly UserManager<AppUser> _userManager;
     private readonly ITokenService _tokenService;
     private readonly IMapper _mapper;
 
     public AccountController(
-        DataContext context,
+        UserManager<AppUser> userManager,
         ITokenService tokenService,
         IMapper mapper)
     {
-        _context = context;
+        _userManager = userManager;
         _tokenService = tokenService;
         _mapper = mapper;
     }
@@ -33,14 +34,11 @@ public class AccountController : BaseApiController
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
         var user = _mapper.Map<AppUser>(registerDto);
-        using var hmac = new HMACSHA512();
-
         user.UserName = registerDto.Username.ToLower();
-        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-        user.PasswordSalt = hmac.Key; // randomly generates a salt
 
-        _context.Users.Add(user);
-        await _context.SaveChangesAsync();
+        var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded) return BadRequest(result.Errors);
 
         return new UserDto
         {
@@ -56,20 +54,15 @@ public class AccountController : BaseApiController
     {
         var username = loginDto.Username.ToLower();
 
-        var user = await _context.Users
+        var user = await _userManager.Users
             .Include(p => p.Photos)
             .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
         if (user == null) return Unauthorized("Invalid username");
 
-        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
 
-        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-        // computedHash and user.PasswordHash are byte arrays
-        for (int i = 0; i < computedHash.Length; i++)
-        {
-            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-        }
+        if (!result) return Unauthorized("Invalid password");
 
         return new UserDto
         {
@@ -81,9 +74,8 @@ public class AccountController : BaseApiController
         };
     }
 
-
     private async Task<bool> UserExists(string username)
     {
-        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
 }
